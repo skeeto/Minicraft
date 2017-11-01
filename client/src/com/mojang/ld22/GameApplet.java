328afd2c95d8378a7863dba0b5881da6c3f0d430
ppartarr <ppartarr@outlook.com>
2017-11-01 18:27:18 +0000

    returned client to original state

--- client/src/com/mojang/ld22/GameApplet.java
+++ client/src/com/mojang/ld22/GameApplet.java
@@ -2,9 +2,6 @@ package com.mojang.ld22;
 
 import java.applet.Applet;
 import java.awt.BorderLayout;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 
 public class GameApplet extends Applet {
 	private static final long serialVersionUID = 1L;
@@ -19,18 +16,6 @@ public class GameApplet extends Applet {
 	public void start() {
 		try {
 			game.start();
-			InputHandler obj = new InputHandler(game);
-			InputHandlerInterface stub = (InputHandlerInterface) UnicastRemoteObject.exportObject(obj, 1234);
-
-			// Bind the remote object's stub in the registry
-			Registry registry = LocateRegistry.getRegistry();
-			registry.bind("InputHandler", stub);
-			System.err.println("Server ready");
-
-		} catch (Exception e) {
-			System.err.println("Server exception: " + e.toString());
-			e.printStackTrace();
-		}
 	}
 
 	public void stop() {
